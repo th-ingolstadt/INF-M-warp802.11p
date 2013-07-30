d3bd9ea9da8d169946e289e6abf5533c3ae074c4
chunter <chunter@dd934ca3-7c2e-414f-9084-136bfdd87501>
2013-07-30 19:17:46 +0000

    raise rssi pkt det thresh
    git-svn-id: http://warpproject.org/svn/WARP@2108 dd934ca3-7c2e-414f-9084-136bfdd87501

--- ReferenceDesigns/w3_802.11/c/wlan_mac_dcf/wlan_phy_util.c
+++ ReferenceDesigns/w3_802.11/c/wlan_mac_dcf/wlan_phy_util.c
@@ -106,7 +106,7 @@ void wlan_phy_init() {
 	wlan_phy_rx_lts_corr_cfg(30000, 250);
 
 	//Configure RSSI pkt det
-	wlan_phy_rx_pktDet_RSSI_cfg(8, (8*1023), 4); //Disable RSSI pkt det with high thresh
+	wlan_phy_rx_pktDet_RSSI_cfg(8, 0xFFFF, 4); //Disable RSSI pkt det with high thresh
 	//wlan_phy_rx_pktDet_RSSI_cfg(8, (8*300), 8); //Disable RSSI pkt det with high thresh
 	
 	//Configure auto-corr pkt det
