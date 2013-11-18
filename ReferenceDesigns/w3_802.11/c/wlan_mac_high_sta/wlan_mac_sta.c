4ecda5ac3520fcc8eb8d009abb16803e7d0cd7b9
chunter <chunter@dd934ca3-7c2e-414f-9084-136bfdd87501>
2013-11-18 23:14:16 +0000

    git-svn-id: http://warpproject.org/svn/WARP@2408 dd934ca3-7c2e-414f-9084-136bfdd87501

--- ReferenceDesigns/w3_802.11/c/wlan_mac_high_sta/wlan_mac_sta.c
+++ ReferenceDesigns/w3_802.11/c/wlan_mac_high_sta/wlan_mac_sta.c
@@ -48,7 +48,6 @@
 #define  WLAN_EXP_ETH                  WN_ETH_B
 #define  WLAN_EXP_TYPE                 WARPNET_TYPE_80211_BASE + WARPNET_TYPE_80211_STATION
 
-
 #define  WLAN_CHANNEL                  1
 
 
