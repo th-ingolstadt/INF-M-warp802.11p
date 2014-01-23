8ff523eb2b75943fc1aab8d124f550dbddc40d23
welsh <welsh@dd934ca3-7c2e-414f-9084-136bfdd87501>
2014-01-23 17:56:29 +0000

    git-svn-id: http://warpproject.org/svn/WARP@2636 dd934ca3-7c2e-414f-9084-136bfdd87501

--- ReferenceDesigns/w3_802.11/wlan_exp/warpnet/classes/wn_msg_helper.m
+++
@@ -1,9 +0,0 @@
-classdef wn_msg_helper < handle_light
-    properties (Abstract)
-    end
-    methods (Abstract)
-        out = serialize(varargin);
-        out = deserialize(varargin);
-        out = sizeof(varargin);
-    end 
-end
