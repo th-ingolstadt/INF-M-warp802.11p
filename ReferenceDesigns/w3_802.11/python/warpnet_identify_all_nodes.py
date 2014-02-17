9c2504040daff564d90302e3a9a853adfbdb4fef
welsh <welsh@dd934ca3-7c2e-414f-9084-136bfdd87501>
2014-02-17 19:58:19 +0000

    New script to blink the LEDs of all nodes.
    git-svn-id: http://warpproject.org/svn/WARP@2809 dd934ca3-7c2e-414f-9084-136bfdd87501

---
+++ ReferenceDesigns/w3_802.11/python/warpnet_identify_all_nodes.py
@@ -0,0 +1,25 @@
+"""
+This script blinks the red LEDs of all nodes to ensure broadcast communication
+
+Hardware Setup:
+ - Requires one or more WARP v3 nodes configured with the 802.11 Reference
+   Design v0.81 or later.
+ - PC NIC and ETH B on WARP v3 nodes connected to common gigbit Ethernet switch
+
+Required Script Changes:
+  - Set HOST_INTERFACES to the IP address of your host PC NIC
+
+Description:
+  This script will cause all nodes on each of the host interfaces to 
+blink their LEDs.
+"""
+import time
+import warpnet.wn_util as wn_util
+
+# NOTE: change these values to match your experiment setup
+HOST_INTERFACES   = ['10.0.0.250']
+
+# Issue identify all command
+#   Wait 1 second before issuing the command to make sure all nodes are ready
+time.sleep(1)
+wn_util.wn_identify_all_nodes(HOST_INTERFACES)
