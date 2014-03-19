cf2a05f859d5350807cf02e31e00773b6bd86059
chunter <chunter@dd934ca3-7c2e-414f-9084-136bfdd87501>
2014-03-19 18:51:44 +0000

    adopting new dl_entry conventions for station_info
    git-svn-id: http://warpproject.org/svn/WARP@3050 dd934ca3-7c2e-414f-9084-136bfdd87501

--- ReferenceDesigns/w3_802.11/c/wlan_mac_common/include/wlan_mac_misc_util.h
+++ ReferenceDesigns/w3_802.11/c/wlan_mac_common/include/wlan_mac_misc_util.h
@@ -54,6 +54,12 @@ typedef struct{
 } mac_tx_params;
 
 
+///Compile-time assertions
+#define CASSERT(predicate, file) _impl_CASSERT_LINE(predicate,__LINE__,file)
+
+#define _impl_PASTE(a,b) a##b
+#define _impl_CASSERT_LINE(predicate, line, file) \
+    typedef char _impl_PASTE(assertion_failed_##file##_,line)[2*!!(predicate)-1];
 
 
 /**
