4ab0b90a132a6b6089a26f5c90385419b5d05992
chunter <chunter@dd934ca3-7c2e-414f-9084-136bfdd87501>
2014-04-01 21:21:14 +0000

    git-svn-id: http://warpproject.org/svn/WARP@3206 dd934ca3-7c2e-414f-9084-136bfdd87501

--- ReferenceDesigns/w3_802.11/c/wlan_mac_high_ap/wlan_mac_ap.c
+++ ReferenceDesigns/w3_802.11/c/wlan_mac_high_ap/wlan_mac_ap.c
@@ -199,6 +199,7 @@ int main(){
 	mac_param_chan = WLAN_CHANNEL;
 	wlan_mac_high_set_channel( mac_param_chan );
 	wlan_mac_high_set_rx_ant_mode(RX_ANTMODE_SISO_ANTA);
+	wlan_mac_high_set_tx_ctrl_pow(TX_POWER_DBM);
 
 
 	// Set SSID
