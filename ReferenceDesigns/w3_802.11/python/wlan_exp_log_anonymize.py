d3ea3a5ac519a7830d4d30eefc6a7a04795c6afc
welsh <welsh@dd934ca3-7c2e-414f-9084-136bfdd87501>
2014-04-02 05:21:41 +0000

    Updated script to fully anonymize logs with all the new entry types.
    git-svn-id: http://warpproject.org/svn/WARP@3221 dd934ca3-7c2e-414f-9084-136bfdd87501

--- ReferenceDesigns/w3_802.11/python/wlan_exp_log_anonymize.py
+++ ReferenceDesigns/w3_802.11/python/wlan_exp_log_anonymize.py
@@ -1,25 +1,66 @@
+"""
+This script uses the WLAN Exp Log framework to anonymize a given hdf5
+log file that contains data assocated with an experiment utilizing the
+802.11 reference design and WARPNet.
+
+Hardware Setup:
+    - None.  Anonymizing log data can be done completely off-line
+
+Required Script Changes:
+    - None.  Script requires filename of file to be anonymized to be
+      passed in on the command line.
+
+Description:
+    This script parses the log file, removes any personally identifiable
+    information from the log and write the resulting log data to a new
+    file.  The personally identifiable information that is removed:
+        - Any MAC address that is not in the following categories:
+            - Broadcast Address (ff-ff-ff-ff-ff-ff)
+            - IP v4 Multicast Address (01-00-5E-xx-xx-xx)
+            - IP v6 Multicast Address (33-33-xx-xx-xx-xx)
+            - WARP node (40-D8-55-04-2x-xx-xx)
+        - Any payloads from transmissions / receptions
+        - Any WARPNet commands
+        - Hostnames in the station info
+
+License:   Copyright 2014, Mango Communications. All rights reserved.
+           Distributed under the WARP license (http://warpproject.org/license)
+"""
 import sys
 import os
+import time
 
-import warpnet.wlan_exp_log.log_util as log_util
+import warpnet.wlan_exp_log.log_util     as log_util
 import warpnet.wlan_exp_log.log_util_hdf as hdf_util
+
+# Global flag to print performance data
+print_time = False
+
 
 all_addrs = list()
 addr_idx_map = dict()
 
+
 def do_replace_addr(addr):
     do_replace = True
 
-    #Don't replace the broadcast address (FF-FF-FF-FF-FF-FF)
+    # NOTE:  This list should stay in sync with wlan_exp.util mac_desc_map
+
+    # Don't replace the broadcast address (FF-FF-FF-FF-FF-FF)
     if(addr == (0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF)):
         do_replace = False
 
-    #Don't replace multicast IP addresses (01-00-5E-xx-xx-xx)
-    # http://technet.microsoft.com/en-us/library/cc957928.aspx
+    # Don't replace multicast IP v4 addresses (01-00-5E-xx-xx-xx)
+    #   http://technet.microsoft.com/en-us/library/cc957928.aspx
     if(addr[0:3] == (0x01, 0x00, 0x5E) and (addr[4] <= 0x7F)):
         do_replace = False
 
-    #Don't replace Mango addresses (40-D8-55-04-2X-XX)
+    # Don't replace multicast IP v6 addresses (33-33-xx-xx-xx-xx)
+    #   http://www.cavebear.com/archive/cavebear/Ethernet/multicast.html
+    if(addr[0:2] == (0x33, 0x33)):
+        do_replace = False
+
+    # Don't replace Mango addresses (40-D8-55-04-2x-xx)
     if(addr[0:4] == (0x40, 0xD8, 0x55, 0x04) and ((addr[4] & 0x20) == 0x20)):
         do_replace = False
 
@@ -49,7 +90,7 @@ def log_anonymize(filename):
     log_data_index = hdf_util.hdf5_to_log_data_index(filename=filename)
     
     # Generate the index of log entry locations sorted by log entry type
-    log_index     = log_util.filter_log_index(log_data_index, merge={'RX_ALL': ['RX_OFDM','RX_DSSS']})
+    log_index     = log_util.filter_log_index(log_data_index)
 
     # Re-initialize the address-byteindex map per file using the running
     #   list of known MAC addresses
@@ -59,56 +100,127 @@ def log_anonymize(filename):
 
     log_util.print_log_index_summary(log_index)
 
+
+    #####################
     # Step 1: Build a dictionary of all MAC addresses in the log, then
     #   map each addresses to a unique anonymous address
     #   Uses tuple(bytearray slice) since bytearray isn't hashable as-is
 
-    #Rx entries
-    try:
-        for idx in log_index['RX_ALL']:
-            #6-byte addresses at offsets 12, 18, 24
-            for o in (12, 18, 24):
-                addr_to_replace(tuple(log_bytes[idx+o:idx+o+6]), idx+o, addr_idx_map)
-    except KeyError:
-        pass
+    print("Anonmyizing file step 1 ...")
 
-    #Tx entries
-    try:
-        for idx in log_index['TX']:
-            #6-byte addresses at offsets 20, 26, 32
-            for o in (20, 26, 32):
-                addr_to_replace(tuple(log_bytes[idx+o:idx+o+6]), idx+o, addr_idx_map)
-    except KeyError:
-        pass
+    start_time = time.time()
 
-    #Station Info entries
+    # Station Info entries
+    print("    STATION_INFO entries")
     try:
         for idx in log_index['STATION_INFO']:
-            #6-byte address at offsets 8
+            # 6-byte address at offsets 8
                 o = 8
                 addr_to_replace(tuple(log_bytes[idx+o:idx+o+6]), idx+o, addr_idx_map)
     except KeyError:
         pass
 
-    #TODO: Add WN cmd entries, to cover LTG cmds that might have non-Mango MAC addresses?
+    if print_time:
+        print("        Time = {0:.3f}s".format(time.time() - start_time))
+
+    # Tx/Rx Statistics entries
+    print("    TXRX_STATS entries")
+    try:
+        for idx in log_index['TXRX_STATS']:
+            # 6-byte addresses at offsets 16
+                o = 16
+                addr_to_replace(tuple(log_bytes[idx+o:idx+o+6]), idx+o, addr_idx_map)
+    except KeyError:
+        pass
+
+    if print_time:
+        print("        Time = {0:.3f}s".format(time.time() - start_time))
+
+    # Rx DSSS entries
+    print("    RX_DSSS entries")
+    try:
+        for idx in log_index['RX_DSSS']:
+            # 6-byte addresses at offsets 28, 34, 40
+            for o in (28, 34, 40):
+                addr_to_replace(tuple(log_bytes[idx+o:idx+o+6]), idx+o, addr_idx_map)
+    except KeyError:
+        pass
+
+    if print_time:
+        print("        Time = {0:.3f}s".format(time.time() - start_time))
+
+    # Rx OFDM entries
+    print("    RX_OFDM entries")
+    try:
+        for idx in log_index['RX_OFDM']:
+            # 6-byte addresses at offsets 284, 290, 296
+            for o in (284, 290, 296):
+                addr_to_replace(tuple(log_bytes[idx+o:idx+o+6]), idx+o, addr_idx_map)
+    except KeyError:
+        pass
+
+    if print_time:
+        print("        Time = {0:.3f}s".format(time.time() - start_time))
+
+    # Tx entries
+    print("    TX entries")
+    try:
+        for idx in log_index['TX']:
+            # 6-byte addresses at offsets 44, 50, 56
+            for o in (44, 50, 56):
+                addr_to_replace(tuple(log_bytes[idx+o:idx+o+6]), idx+o, addr_idx_map)
+    except KeyError:
+        pass
+
+    if print_time:
+        print("        Time = {0:.3f}s".format(time.time() - start_time))
+
+    # Tx Low entries
+    print("    TX_LOW entries")
+    try:
+        for idx in log_index['TX_LOW']:
+            # 6-byte addresses at offsets 40, 46, 52
+            for o in (40, 46, 52):
+                addr_to_replace(tuple(log_bytes[idx+o:idx+o+6]), idx+o, addr_idx_map)
+    except KeyError:
+        pass
+
+    if print_time:
+        print("        Time = {0:.3f}s".format(time.time() - start_time))
 
     #####################
-    #Step 2: Enumerate actual MAC addresses and their anonymous replacements
+    # Step 2: Enumerate actual MAC addresses and their anonymous replacements
+
+    print("Anonmyizing file step 2 ...")
+
     addr_map = dict()
     for ii,addr in enumerate(all_addrs):
         anon_addr = (0xFF, 0xFF, 0xFF, 0xFF, (ii//256), (ii%256))
         addr_map[addr] = anon_addr
 
-    #Step 3: Replace all MAC addresses in the log 
+    if print_time:
+        print("        Time = {0:.3f}s".format(time.time() - start_time))
+
+    #####################
+    # Step 3: Replace all MAC addresses in the log 
+
+    print("Anonmyizing file step 3 ...")
+
     for old_addr in addr_idx_map.keys():
         new_addr = bytearray(addr_map[old_addr])
         for byte_idx in addr_idx_map[old_addr]:
             log_bytes[byte_idx:byte_idx+6] = new_addr
 
-    #Step 4: Other annonymization steps
+    if print_time:
+        print("        Time = {0:.3f}s".format(time.time() - start_time))
 
-    #Station info entries contain "hostname", the DHCP client hostname field
-    # Replace these with a string version of the new anonymous MAC addr
+    #####################
+    # Step 4: Other annonymization steps
+
+    print("Anonmyizing file step 4 ...")
+
+    # Station info entries contain "hostname", the DHCP client hostname field
+    #   Replace these with a string version of the new anonymous MAC addr
     try:
         for idx in log_index['STATION_INFO']:
             #6-byte MAC addr (already anonymized) at offset 8
@@ -122,6 +234,25 @@ def log_anonymize(filename):
     except KeyError:
         pass
 
+    # WARPNet Command info entries contain command arguments that could possibly 
+    #   contain sensitive information.  Replace with NULL entries.
+    try:
+        log_util.overwrite_entries_with_null_entry(log_bytes, log_index['WN_CMD_INFO'])
+    except:
+        pass
+
+    # Overwrite all payloads with zeros
+    try:
+        for key in log_index.keys():
+            log_util.overwrite_payloads(log_bytes, log_index[key])
+    except:
+        pass
+
+    if print_time:
+        print("        Time = {0:.3f}s".format(time.time() - start_time))
+
+    #####################
+    # Write output files
 
     #Write the modified log to a new HDF5 file
     (fn_fldr, fn_file) = os.path.split(filename)
@@ -137,6 +268,9 @@ def log_anonymize(filename):
         fn_base = fn_file
 
     newfilename = os.path.join(fn_fldr, fn_base + "_anon" + fn_ext)
+
+    print("Writing new file {0} ...".format(newfilename))
+
     hdf_util.log_data_to_hdf5(log_bytes, newfilename)
 
     return
