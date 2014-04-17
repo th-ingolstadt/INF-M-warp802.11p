e83457fcaa9ceed038606ca73ea2b1ad5eb50ba0
welsh <welsh@dd934ca3-7c2e-414f-9084-136bfdd87501>
2014-04-17 17:16:55 +0000

    Updated script so that it only runs when called explicitly.
    git-svn-id: http://warpproject.org/svn/WARP@3437 dd934ca3-7c2e-414f-9084-136bfdd87501

--- ReferenceDesigns/w3_802.11/python/wlan_exp/log/gen_entry_type_wiki_docs.py
+++ ReferenceDesigns/w3_802.11/python/wlan_exp/log/gen_entry_type_wiki_docs.py
@@ -1,6 +1,11 @@
-from wlan_exp.log.entry_types import log_entry_types as log_entry_types
+"""
+Create wiki documentation from entry types
+"""
 
-for tid in log_entry_types.keys():
-    if(type(tid) is int and tid != 0):
-        print(log_entry_types[tid].generate_entry_doc(fmt='wiki'))
+if __name__ == '__main__':
+    from wlan_exp.log.entry_types import log_entry_types as log_entry_types
+    
+    for tid in log_entry_types.keys():
+        if(type(tid) is int and tid != 0):
+            print(log_entry_types[tid].generate_entry_doc(fmt='wiki'))
 
