776bc923acc1af6426af0effa30e474952ec7241
chunter <chunter@dd934ca3-7c2e-414f-9084-136bfdd87501>
2014-04-30 22:58:10 +0000

    git-svn-id: http://warpproject.org/svn/WARP@3485 dd934ca3-7c2e-414f-9084-136bfdd87501

--- ReferenceDesigns/w3_802.11/c/wlan_mac_common/include/wlan_mac_misc_util.h
+++ ReferenceDesigns/w3_802.11/c/wlan_mac_common/include/wlan_mac_misc_util.h
@@ -22,7 +22,7 @@ typedef int (*function_ptr_t)();
 #define max(A,B) (((A)>(B))?(A):(B))
 #define min(A,B) (((A)<(B))?(A):(B))
 
-#define _ISR_PERF_MON_EN_	///< ISR Performance Monitor Toggle
+//#define _ISR_PERF_MON_EN_	///< ISR Performance Monitor Toggle
 #define ISR_PERF_MON_GPIO_MASK	0x01
 
 #define PRINT_LEVEL PL_ERROR
