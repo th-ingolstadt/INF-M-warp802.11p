c90ce7e3f2e1851e54677a73f47058eebd0586ff
chunter <chunter@dd934ca3-7c2e-414f-9084-136bfdd87501>
2014-05-03 21:15:28 +0000

    disabled bounds checking for speedup
    git-svn-id: http://warpproject.org/svn/WARP@3522 dd934ca3-7c2e-414f-9084-136bfdd87501

--- ReferenceDesigns/w3_802.11/python/wlan_exp/log/coll_util.pyx
+++ ReferenceDesigns/w3_802.11/python/wlan_exp/log/coll_util.pyx
@@ -1,6 +1,7 @@
 #cython: boundscheck=False
 #cython: wraparound=False
-
+cimport cython
+@cython.boundscheck(False) # turn of bounds-checking for entire function
 
 import numpy as np
 cimport numpy as np
@@ -14,15 +15,10 @@ def _collision_idx_finder_l(np.ndarray[DTYPE_t, ndim=1] src_ts, np.ndarray[DTYPE
     import wlan_exp.util as wlan_exp_util    
     
     assert src_ts.dtype == DTYPE and src_dur.dtype == DTYPE and int_ts.dtype == DTYPE and int_dur.dtype == DTYPE
-    
-
-    #src_coll_idx = []
-    #int_coll_idx = []
   
     cdef int num_src = src_ts.shape[0]
     cdef int num_int = src_ts.shape[0]
     cdef DTYPE_t num_coll = 0
-    #num_coll = 0
     
     #Unfortunately, at this stage we don't know how many collisions there are, so we have to assume
     #the worst for sizing these arrays -- every single tx_low collided.
@@ -35,15 +31,17 @@ def _collision_idx_finder_l(np.ndarray[DTYPE_t, ndim=1] src_ts, np.ndarray[DTYPE
     cdef DTYPE_t curr_int_ts
     cdef DTYPE_t curr_int_dur
     
-#    int_ts_end = int_ts + int_dur
+    int_ts_end = int_ts + int_dur
     
-#    for src_idx in range(len(src_ts)):
-#        #if src_idx % 100 == 0:
-#        print("{0}/{1}".format(src_idx,len(src_ts)))
-            
-#        coll_vec = (int_ts < (src_ts[src_idx] + src_dur[src_idx]) ) & (src_ts[src_idx] < int_ts_end)    
+#    for src_idx in range(num_src):
+#        if src_idx % 10 == 0:
+#            print("{0}/{1}".format(src_idx,len(src_ts)))
+#            
+#        coll_vec = (int_ts < (src_ts[src_idx] + src_dur[src_idx]) ) & (src_ts[src_idx] < int_ts_end )    
 #        if(sum(coll_vec) > 0):
-#            src_coll_idx = np.append(src_coll_idx, src_idx)
+#            src_coll_idx[num_coll] = src_idx
+#            int_coll_idx[num_coll] = 0                
+#            num_coll = num_coll + 1
 #            continue
             
     for src_idx in range(num_src):
@@ -62,14 +60,10 @@ def _collision_idx_finder_l(np.ndarray[DTYPE_t, ndim=1] src_ts, np.ndarray[DTYPE
                 int_coll_idx[num_coll] = int_idx                
                 num_coll = num_coll + 1
                 continue
-                
-#                src_coll_idx = np.append(src_coll_idx, src_idx)
-#                int_coll_idx = np.append(int_coll_idx, int_idx)
-                
+                                
         
         
-    print("{0} Detected overlapping transmissions".format(num_coll))
-#    return num_coll    
+    print("{0} detected overlapping transmissions".format(num_coll))
 
     src_coll_idx = src_coll_idx[0:num_coll]
     int_coll_idx = int_coll_idx[0:num_coll]    
