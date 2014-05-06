e6c42e5b63ca356a308e558c6d4ce1a9530094f5
chunter <chunter@dd934ca3-7c2e-414f-9084-136bfdd87501>
2014-05-06 18:53:35 +0000

    git-svn-id: http://warpproject.org/svn/WARP@3532 dd934ca3-7c2e-414f-9084-136bfdd87501

--- ReferenceDesigns/w3_802.11/python/wlan_exp/log/coll_util.py
+++ ReferenceDesigns/w3_802.11/python/wlan_exp/log/coll_util.py
@@ -7,6 +7,76 @@ def _collision_idx_finder_l(src_ts, src_dur, int_ts, int_dur):
     num_src = src_ts.shape[0]
     num_int = int_ts.shape[0]
     
+#    maxlen = np.max([num_src,num_int])
+    maxlen = num_src+num_int
+    src_coll_idx = np.zeros([maxlen],dtype=np.int)
+    int_coll_idx = np.zeros([maxlen],dtype=np.int)
+    
+    int_idx_start = 0
+    
+    coll_index = 0;
+    
+    for src_idx in range(num_src):        
+        
+        curr_src_ts = src_ts[src_idx]
+        curr_src_dur = src_dur[src_idx] 
+        
+        curr_src_tx_end = curr_src_ts + curr_src_dur
+        
+        num_iter = 0
+        
+        int_idx = num_int/2
+        int_idx_high = num_int-1
+        int_idx_low = 0
+
+        #start in middle and branch out   
+        while 1:
+            num_iter = num_iter+1
+            if (int_idx == int_idx_low or int_idx == int_idx_high):
+                #We're done. No overlap
+                break            
+            
+            curr_int_ts = int_ts[int_idx]
+            curr_int_dur = int_dur[int_idx]
+            
+            if ( curr_int_ts < (curr_src_tx_end) ) and ( curr_src_ts < (curr_int_ts + curr_int_dur) ):
+                            
+                #We found an overlap
+                src_coll_idx[coll_index] = src_idx
+                int_coll_idx[coll_index] = int_idx 
+                coll_index = coll_index+1         
+                
+                #Keep iterating forward on int_idx until there isn't an overlap                                                        
+                while int_idx < (num_int-1):
+                    int_idx = int_idx+1
+                    curr_int_ts = int_ts[int_idx]
+                    curr_int_dur = int_dur[int_idx]                    
+                    if ( curr_int_ts < (curr_src_tx_end) ) and ( curr_src_ts < (curr_int_ts + curr_int_dur) ):
+                        #Found another overlap
+                        src_coll_idx[coll_index] = src_idx
+                        int_coll_idx[coll_index] = int_idx 
+                        coll_index = coll_index+1                             
+                    else:
+                        #No more collisions, move on
+                        break                        
+                break
+            elif ( curr_int_ts < curr_src_ts ):
+                #Overlap may be later -- move index forward
+                int_idx_low = int_idx                
+            else:
+                #Overlap may be earlier -- move index back
+                int_idx_high = int_idx
+
+            int_idx = (int_idx_high - int_idx_low)/2 + int_idx_low 
+                                                            
+        
+    return (src_coll_idx,int_coll_idx)
+
+def _collision_idx_finder_l_alt(src_ts, src_dur, int_ts, int_dur):
+     
+    num_src = src_ts.shape[0]
+    num_int = int_ts.shape[0]
+    
     maxlen = np.max([num_src,num_int])
     src_coll_idx = np.zeros([maxlen],dtype=np.int)
     int_coll_idx = np.zeros([maxlen],dtype=np.int)
@@ -22,57 +92,21 @@ def _collision_idx_finder_l(src_ts, src_dur, int_ts, int_dur):
         
         num_iter = 0
         
-#%% Inexplicably, this technique is quite a bit slower than the binary search.        
-#        for int_idx in range(int_idx_start,num_int):
-##            print("{0}:[{1} {2} {3}]".format(src_idx,int_idx_start,int_idx,num_int))
-#            num_iter = num_iter+1
-#            curr_int_ts = int_ts[int_idx]
-#            curr_int_dur = int_dur[int_idx]
-#
-#            if ( curr_int_ts < (curr_src_tx_end) ) and ( curr_src_ts < (curr_int_ts + curr_int_dur) ):                
-#                #Found an overlap
-#                src_coll_idx[src_idx] = src_idx
-#                int_coll_idx[src_idx] = int_idx 
-#                int_idx_start = int_idx;
-#                #TODO: We should check the next packet to see if it also collided        
-#                break
-#            elif ( curr_int_ts > (curr_src_tx_end) ):
-#                int_idx_start = int_idx;
-#                break
 
-#%%
-        int_idx = num_int/2
-        int_idx_high = num_int-1
-        int_idx_low = 0
-
-        #start in middle and branch out   
-        while 1:
+        for int_idx in range(int_idx_start,num_int):
             num_iter = num_iter+1
-            if (int_idx == int_idx_low or int_idx == int_idx_high):
-                #We're done. No overlap
-                break            
-
-#            print("{0}: [{1} {2} {3}] [{4}]".format(src_idx, int_idx_low, int_idx, int_idx_high, num_int))            
-            
             curr_int_ts = int_ts[int_idx]
             curr_int_dur = int_dur[int_idx]
-            
-            if ( curr_int_ts < (curr_src_tx_end) ) and ( curr_src_ts < (curr_int_ts + curr_int_dur) ):
-                #We found an overlap
-                #TODO: keep iterating forward on int_idx until there isn't an overlap
+
+            if ( curr_int_ts < (curr_src_tx_end) ) and ( curr_src_ts < (curr_int_ts + curr_int_dur) ):                
+                #Found an overlap
                 src_coll_idx[src_idx] = src_idx
                 int_coll_idx[src_idx] = int_idx 
+                int_idx_start = int_idx;
+                #TODO: We should check the next packet to see if it also collided        
                 break
-            elif ( curr_int_ts < curr_src_ts ):
-                #Overlap may be later -- move index forward
-                int_idx_low = int_idx                
-            else:
-                #Overlap may be earlier -- move index back
-                int_idx_high = int_idx
-
-            int_idx = (int_idx_high - int_idx_low)/2 + int_idx_low
-#%%        
-#        print("{0}: num_iter = {1}".format(src_idx, num_iter))                                                                
+            elif ( curr_int_ts > (curr_src_tx_end) ):
+                int_idx_start = int_idx;
+                break                                                    
         
     return (src_coll_idx,int_coll_idx)
-
