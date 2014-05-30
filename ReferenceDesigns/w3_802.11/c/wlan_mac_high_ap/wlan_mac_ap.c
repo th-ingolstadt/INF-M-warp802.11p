0a970cba17944c2864cbf5e32f91d33c648d96ea
chunter <chunter@dd934ca3-7c2e-414f-9084-136bfdd87501>
2014-05-30 19:16:43 +0000

    git-svn-id: http://warpproject.org/svn/WARP@3608 dd934ca3-7c2e-414f-9084-136bfdd87501

--- ReferenceDesigns/w3_802.11/c/wlan_mac_high_ap/wlan_mac_ap.c
+++ ReferenceDesigns/w3_802.11/c/wlan_mac_high_ap/wlan_mac_ap.c
@@ -192,7 +192,7 @@ int main(){
 	// Configure default radio and PHY params via messages to CPU Low
 	mac_param_chan = WLAN_DEFAULT_CHANNEL;
 	wlan_mac_high_set_channel( mac_param_chan );
-	wlan_mac_high_set_rx_ant_mode(RX_ANTMODE_SISO_ANTA);
+	wlan_mac_high_set_rx_ant_mode(RX_ANTMODE_SISO_SELDIV_2ANT);
 	wlan_mac_high_set_tx_ctrl_pow(WLAN_DEFAULT_TX_PWR);
 
 	// Configure CPU Low's filter for passing Rx packets up to CPU High
