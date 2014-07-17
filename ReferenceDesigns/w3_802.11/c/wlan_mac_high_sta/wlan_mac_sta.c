656f07163f43e392cc07135635648be403dba91a
welsh <welsh@dd934ca3-7c2e-414f-9084-136bfdd87501>
2014-07-17 19:40:50 +0000

    Updates to revision numbers / default values
    git-svn-id: http://warpproject.org/svn/WARP@3728 dd934ca3-7c2e-414f-9084-136bfdd87501

--- ReferenceDesigns/w3_802.11/c/wlan_mac_high_sta/wlan_mac_sta.c
+++ ReferenceDesigns/w3_802.11/c/wlan_mac_high_sta/wlan_mac_sta.c
@@ -136,7 +136,7 @@ int main() {
 	// Print initial message to UART
 	xil_printf("\f");
 	xil_printf("----- Mango 802.11 Reference Design -----\n");
-	xil_printf("----- v0.9 Beta -------------------------\n");
+	xil_printf("----- v0.95 Beta ------------------------\n");
 	xil_printf("----- wlan_mac_sta ----------------------\n");
 	xil_printf("Compiled %s %s\n\n", __DATE__, __TIME__);
 
