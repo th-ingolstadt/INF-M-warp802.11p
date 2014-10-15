e4d7bdfa683782edcec424cf95521ac670764d35
chunter <chunter@dd934ca3-7c2e-414f-9084-136bfdd87501>
2014-10-15 13:58:30 +0000

    modified where Tx queues are polled and where the framework marks a packet buffer as free
    git-svn-id: http://warpproject.org/svn/WARP@3974 dd934ca3-7c2e-414f-9084-136bfdd87501

--- ReferenceDesigns/w3_802.11/c/wlan_mac_high_ibss/wlan_mac_ibss.c
+++ ReferenceDesigns/w3_802.11/c/wlan_mac_high_ibss/wlan_mac_ibss.c
@@ -126,17 +126,9 @@ int main() {
 	// Print initial message to UART
 	xil_printf("\f");
 	xil_printf("----- Mango 802.11 Reference Design -----\n");
-	xil_printf("----- v0.96 Beta ------------------------\n");
-	xil_printf("----- wlan_mac_ibss ----------------------\n");
+	xil_printf("----- v1.0 ------------------------------\n");
+	xil_printf("----- wlan_mac_ibss ---------------------\n");
 	xil_printf("Compiled %s %s\n\n", __DATE__, __TIME__);
-
-
-	// Check that right shift works correctly
-	//   Issue with -Os in Xilinx SDK 14.7
-	if (wlan_mac_high_right_shift_test() != 0) {
-		wlan_mac_high_set_node_error_status(0);
-		wlan_mac_high_blink_hex_display(0, 250000);
-	}
 
 	// This function should be executed first. It will zero out memory, and if that
 	//     memory is used before calling this function, unexpected results may happen.
@@ -187,13 +179,13 @@ int main() {
 #endif
 
 	// Initialize callbacks
-	wlan_mac_util_set_eth_rx_callback(       (void*)ethernet_receive);
-	wlan_mac_high_set_mpdu_tx_done_callback( (void*)mpdu_transmit_done);
-	wlan_mac_high_set_mpdu_dequeue_callback( (void*)mpdu_dequeue);
-	wlan_mac_high_set_mpdu_rx_callback(      (void*)mpdu_rx_process);
-	wlan_mac_high_set_uart_rx_callback(      (void*)uart_rx);
-	wlan_mac_high_set_mpdu_accept_callback(  (void*)poll_tx_queues);
-	wlan_mac_ltg_sched_set_callback(         (void*)ltg_event);
+	wlan_mac_util_set_eth_rx_callback(       	(void*)ethernet_receive);
+	wlan_mac_high_set_mpdu_tx_done_callback( 	(void*)mpdu_transmit_done);
+	wlan_mac_high_set_mpdu_dequeue_callback( 	(void*)mpdu_dequeue);
+	wlan_mac_high_set_mpdu_rx_callback(      	(void*)mpdu_rx_process);
+	wlan_mac_high_set_uart_rx_callback(      	(void*)uart_rx);
+	wlan_mac_high_set_poll_tx_queues_callback(  (void*)poll_tx_queues);
+	wlan_mac_ltg_sched_set_callback(         	(void*)ltg_event);
 
 	// Set the Ethernet ecapsulation mode
 	wlan_mac_util_set_eth_encap_mode(ENCAP_MODE_IBSS);
@@ -374,8 +366,6 @@ void beacon_transmit(u32 schedule_id) {
 			// Put the packet in the queue
 			enqueue_after_tail(BEACON_QID, curr_tx_queue_element);
 
-			// Poll the TX queues to possibly send the packet
-			poll_tx_queues();
 		}
 	}
 }
@@ -646,9 +636,6 @@ int ethernet_receive(tx_queue_element* curr_tx_queue_element, u8* eth_dest, u8* 
 			// Put the packet in the queue
 			enqueue_after_tail(queue_sel, curr_tx_queue_element);
 
-			// Poll the TX queues to possibly send the packet
-			poll_tx_queues();
-
 		} else {
 			// Packet was not successfully enqueued
 			return 0;
@@ -839,8 +826,6 @@ void mpdu_rx_process(void* pkt_buf_addr, u8 rate, u16 length) {
 								// Put the packet in the queue
 								enqueue_after_tail(MANAGEMENT_QID, curr_tx_queue_element);
 
-								// Poll the TX queues to possibly send the packet
-								poll_tx_queues();
 							}
 
 							// Finish the function
@@ -1103,7 +1088,7 @@ void ltg_event(u32 id, void* callback_arg){
 
 					// Submit the new packet to the appropriate queue
 					enqueue_after_tail(queue_sel, curr_tx_queue_element);
-					poll_tx_queues();
+
 
 				} else {
 					// There aren't any free queue elements right now.
