af9ba29955aea88a486ed5f95cd6114c70348dfb
welsh <welsh@dd934ca3-7c2e-414f-9084-136bfdd87501>
2014-10-21 20:43:36 +0000

    Adding framework version to entry type wiki generator.
    git-svn-id: http://warpproject.org/svn/WARP@4070 dd934ca3-7c2e-414f-9084-136bfdd87501

--- ReferenceDesigns/w3_802.11/python/wlan_exp/log/gen_entry_type_wiki_docs.py
+++ ReferenceDesigns/w3_802.11/python/wlan_exp/log/gen_entry_type_wiki_docs.py
@@ -3,7 +3,11 @@ Create wiki documentation from entry types
 """
 
 if __name__ == '__main__':
+    import wlan_exp.version as version
     from wlan_exp.log.entry_types import log_entry_types as log_entry_types
+    
+    
+    print("== {0} Log Entry Types ==".format(version.version))
     
     for tid in log_entry_types.keys():
         if(type(tid) is int and tid != 0):
