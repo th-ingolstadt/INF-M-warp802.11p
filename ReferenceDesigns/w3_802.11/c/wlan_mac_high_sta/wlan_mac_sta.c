854bd46cd7379713255d88ea5e3f5f22f476e983
chunter <chunter@dd934ca3-7c2e-414f-9084-136bfdd87501>
2014-12-11 21:40:23 +0000

    1.1 commit
    git-svn-id: http://warpproject.org/svn/WARP@4206 dd934ca3-7c2e-414f-9084-136bfdd87501

--- ReferenceDesigns/w3_802.11/c/wlan_mac_high_sta/wlan_mac_sta.c
+++ ReferenceDesigns/w3_802.11/c/wlan_mac_high_sta/wlan_mac_sta.c
@@ -120,7 +120,7 @@ int main() {
 	// Print initial message to UART
 	xil_printf("\f");
 	xil_printf("----- Mango 802.11 Reference Design -----\n");
-	xil_printf("----- v1.0 ------------------------------\n");
+	xil_printf("----- v1.1 ------------------------------\n");
 	xil_printf("----- wlan_mac_sta ----------------------\n");
 	xil_printf("Compiled %s %s\n\n", __DATE__, __TIME__);
 
