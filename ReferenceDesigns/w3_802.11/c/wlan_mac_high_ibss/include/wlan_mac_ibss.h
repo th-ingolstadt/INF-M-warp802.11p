539534f90e14b6e78d65849ae28f48c450cbc45b
chunter <chunter@dd934ca3-7c2e-414f-9084-136bfdd87501>
2015-02-10 20:03:25 +0000

    updated copyright date to 2015
    git-svn-id: http://warpproject.org/svn/WARP@4377 dd934ca3-7c2e-414f-9084-136bfdd87501

--- ReferenceDesigns/w3_802.11/c/wlan_mac_high_ibss/include/wlan_mac_ibss.h
+++ ReferenceDesigns/w3_802.11/c/wlan_mac_high_ibss/include/wlan_mac_ibss.h
@@ -3,7 +3,7 @@
  *
  *  This contains code for the 802.11 IBSS node (ad hoc).
  *
- *  @copyright Copyright 2014, Mango Communications. All rights reserved.
+ *  @copyright Copyright 2015, Mango Communications. All rights reserved.
  *          Distributed under the Mango Communications Reference Design License
  *				See LICENSE.txt included in the design archive or
  *				at http://mangocomm.com/802.11/license
