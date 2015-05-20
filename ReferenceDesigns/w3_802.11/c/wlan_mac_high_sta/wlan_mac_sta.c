b1d41dff159400607360d9ac4e4a0a635d8f9efb
chunter <chunter@dd934ca3-7c2e-414f-9084-136bfdd87501>
2015-05-20 14:36:17 +0000

    added comment to fix timestamp update
    git-svn-id: http://warpproject.org/svn/WARP@4549 dd934ca3-7c2e-414f-9084-136bfdd87501

--- ReferenceDesigns/w3_802.11/c/wlan_mac_high_sta/wlan_mac_sta.c
+++ ReferenceDesigns/w3_802.11/c/wlan_mac_high_sta/wlan_mac_sta.c
@@ -739,7 +739,7 @@ void mpdu_rx_process(void* pkt_buf_addr, u8 rate, u16 length) {
 							// Calculate the difference between the beacon timestamp and the packet timestamp
 							//     NOTE:  We need to compensate for the time it takes to set the timestamp in the PHY
 
-							timestamp_diff = (s64)(((beacon_probe_frame*)mpdu_ptr_u8)->timestamp) - (s64)(mpdu_info->timestamp) + PHY_T_OFFSET;
+							timestamp_diff = (s64)(((beacon_probe_frame*)mpdu_ptr_u8)->timestamp) - (s64)(mpdu_info->timestamp) + PHY_T_OFFSET; //+ or - 2us //FIXME
 
 							// Set the timestamp
 							wlan_mac_high_set_timestamp_delta(timestamp_diff);
