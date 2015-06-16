97b0815c764b519ba4f58365df4504f4b9e5a0e6
chunter <chunter@dd934ca3-7c2e-414f-9084-136bfdd87501>
2015-06-16 21:24:30 +0000

    updated version numbers and added extra filter control on subtypes of log entries
    git-svn-id: http://warpproject.org/svn/WARP@4597 dd934ca3-7c2e-414f-9084-136bfdd87501

--- ReferenceDesigns/w3_802.11/c/wlan_mac_high_sta/wlan_mac_sta.c
+++ ReferenceDesigns/w3_802.11/c/wlan_mac_high_sta/wlan_mac_sta.c
@@ -119,7 +119,7 @@ int main() {
 	// Print initial message to UART
 	xil_printf("\f");
 	xil_printf("----- Mango 802.11 Reference Design -----\n");
-	xil_printf("----- v1.2 ------------------------------\n");
+	xil_printf("----- v1.3 ------------------------------\n");
 	xil_printf("----- wlan_mac_sta ----------------------\n");
 	xil_printf("Compiled %s %s\n\n", __DATE__, __TIME__);
 
