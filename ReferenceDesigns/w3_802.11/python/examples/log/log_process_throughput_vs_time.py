e8fc96140a07d75c949af01c9519c8feb154cad8
chunter <chunter@dd934ca3-7c2e-414f-9084-136bfdd87501>
2015-06-18 15:30:14 +0000

    added FCS and packet type filters explicitly rather than relying on defaults in what is logged
    git-svn-id: http://warpproject.org/svn/WARP@4608 dd934ca3-7c2e-414f-9084-136bfdd87501

--- ReferenceDesigns/w3_802.11/python/examples/log/log_process_throughput_vs_time.py
+++ ReferenceDesigns/w3_802.11/python/examples/log/log_process_throughput_vs_time.py
@@ -146,7 +146,7 @@ rs_interval = 1 #msec
 rolling_winow = 1000 #samples
 
 # Select non-duplicate packets from partner node
-rx_ap_idx      = (rx_ap['addr2'] == addr_sta) & ((rx_ap['flags'] & 0x1) == 0)
+rx_ap_idx      = (rx_ap['addr2'] == addr_sta) & ((rx_ap['flags'] & 0x1) == 0) & (rx_ap['fcs_result'] == 0) & ( (rx_ap['pkt_type'] == 2) | (rx_ap['pkt_type'] == 3) )
 rx_ap_from_sta = rx_ap[rx_ap_idx]
 
 if (len(rx_ap_from_sta) == 0):
@@ -156,7 +156,7 @@ rx_ap_t        = rx_ap_from_sta['timestamp']
 rx_ap_len      = rx_ap_from_sta['length']
 
 # Select non-duplicate packets from partner node
-rx_sta_idx     = (rx_sta['addr2'] == addr_ap) & ((rx_sta['flags'] & 0x1) == 0)
+rx_sta_idx     = (rx_sta['addr2'] == addr_ap) & ((rx_sta['flags'] & 0x1) == 0)  & (rx_sta['fcs_result'] == 0) & ( (rx_sta['pkt_type'] == 2) | (rx_sta['pkt_type'] == 3) )
 rx_sta_from_ap = rx_sta[rx_sta_idx]
 
 if (len(rx_sta_from_ap) == 0):
