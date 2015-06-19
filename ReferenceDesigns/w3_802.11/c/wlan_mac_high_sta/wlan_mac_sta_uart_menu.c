7f1deb0a90b2c09f513c983493162f5431e697f7
welsh <welsh@dd934ca3-7c2e-414f-9084-136bfdd87501>
2015-06-19 19:56:19 +0000

    Fixing copyright notice to conform more to industry standards.
    git-svn-id: http://warpproject.org/svn/WARP@4620 dd934ca3-7c2e-414f-9084-136bfdd87501

--- ReferenceDesigns/w3_802.11/c/wlan_mac_high_sta/wlan_mac_sta_uart_menu.c
+++ ReferenceDesigns/w3_802.11/c/wlan_mac_high_sta/wlan_mac_sta_uart_menu.c
@@ -3,7 +3,7 @@
  *
  *  This contains code for the 802.11 Station's UART menu.
  *
- *  @copyright Copyright 2014, Mango Communications. All rights reserved.
+ *  @copyright Copyright 2013-2015, Mango Communications. All rights reserved.
  *          Distributed under the Mango Communications Reference Design License
  *				See LICENSE.txt included in the design archive or
  *				at http://mangocomm.com/802.11/license
