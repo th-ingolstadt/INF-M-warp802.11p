1f5b2a1c6a2a91e711d1f641fc13b550a1729a4c
welsh <welsh@dd934ca3-7c2e-414f-9084-136bfdd87501>
2015-06-19 20:22:03 +0000

    Updating versions to 1.3.0.
    git-svn-id: http://warpproject.org/svn/WARP@4627 dd934ca3-7c2e-414f-9084-136bfdd87501

--- ReferenceDesigns/w3_802.11/python/wlan_exp/setup.py
+++ ReferenceDesigns/w3_802.11/python/wlan_exp/setup.py
@@ -13,7 +13,7 @@ def read(*paths):
 
 setup(
     name='mango-wlan-exp',
-    version='1.1.0',
+    version='1.3.0',
     description='Experiments framework for Mango 802.11 Reference Design',
     long_description=(read('README')),
     url='http://warpproject.org/w/802.11/',
