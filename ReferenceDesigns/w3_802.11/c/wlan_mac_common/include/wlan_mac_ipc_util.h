9860449f8a49688545e4d4a297955ba2c4bae0af
chunter <chunter@dd934ca3-7c2e-414f-9084-136bfdd87501>
2015-08-19 15:57:00 +0000

    reverted hopmac
    git-svn-id: http://warpproject.org/svn/WARP@4665 dd934ca3-7c2e-414f-9084-136bfdd87501

--- ReferenceDesigns/w3_802.11/c/wlan_mac_common/include/wlan_mac_ipc_util.h
+++ ReferenceDesigns/w3_802.11/c/wlan_mac_common/include/wlan_mac_ipc_util.h
@@ -63,8 +63,6 @@
 #define IPC_MBOX_MEM_READ_WRITE     	15
 #define IPC_MBOX_LOW_PARAM				16
 #define IPC_MBOX_LOW_RANDOM_SEED        17
-#define IPC_MBOX_CONFIG_HOPPING			50
-#define IPC_MBOX_TOGGLE_HOPPING			51
 
 typedef struct{
 	u32  baseaddr;
