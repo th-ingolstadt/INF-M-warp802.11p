cfdb0cb0ee20bc1aa715f143f0aa17640b965eb1
chunter <chunter@dd934ca3-7c2e-414f-9084-136bfdd87501>
2016-01-14 23:35:50 +0000

    commit for 1.4.3
    git-svn-id: http://warpproject.org/svn/WARP@5041 dd934ca3-7c2e-414f-9084-136bfdd87501

--- ReferenceDesigns/w3_802.11/c/wlan_mac_high_ap/wlan_mac_ap.c
+++ ReferenceDesigns/w3_802.11/c/wlan_mac_high_ap/wlan_mac_ap.c
@@ -126,7 +126,7 @@ int main(){
 
 	xil_printf("\f");
 	xil_printf("----- Mango 802.11 Reference Design -----\n");
-	xil_printf("----- v1.4.2 2XCLK -----------------------\n");
+	xil_printf("----- v1.4.3 2XCLK -----------------------\n");
 	xil_printf("----- wlan_mac_ap -----------------------\n");
 
 	xil_printf("Compiled %s %s\n\n", __DATE__, __TIME__);
