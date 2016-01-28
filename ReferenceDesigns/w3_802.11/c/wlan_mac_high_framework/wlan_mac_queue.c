c4050f64e9fe8d4df7252311ca4ab78b9009fc2d
welsh <welsh@dd934ca3-7c2e-414f-9084-136bfdd87501>
2016-01-28 23:02:52 +0000

    Updated queue_checkout_list() to increase performance (old implementation is #defined out); Added queue_checkin_list()
    git-svn-id: http://warpproject.org/svn/WARP@5102 dd934ca3-7c2e-414f-9084-136bfdd87501

--- ReferenceDesigns/w3_802.11/c/wlan_mac_high_framework/wlan_mac_queue.c
+++ ReferenceDesigns/w3_802.11/c/wlan_mac_high_framework/wlan_mac_queue.c
@@ -290,11 +290,17 @@ void queue_checkin(tx_queue_element* tqe){
  *
  * @return Number of queue entries successfully checked out and appended to new_list
  */
-int queue_checkout_list(dl_list* new_list, u16 num_tqe){
-	//Checks out up to num_packet_bd number of packet_bds from the free list. If num_packet_bd are not free,
-	//then this function will return the number that are free and only check out that many.
+int queue_checkout_list(dl_list* list, u16 num_tqe){
+	// Checks out up to num_tqe number of entries from the free list. If num_tqe are not free,
+	// then this function will return the number that are free and only check out that many.
 
-	u32 i,num_checkout;
+#if 0
+	// Approx. performance metrics:
+	//    incremental processing = 3.3 us / queue entry
+	//    overhead               = 0.3 us
+	//
+	u32       i;
+	u32       num_checkout;
 	dl_entry* curr_dl_entry;
 
 	if(num_tqe <= queue_free.length){
@@ -311,9 +317,173 @@ int queue_checkout_list(dl_list* new_list, u16 num_tqe){
 		dl_entry_remove(&queue_free, curr_dl_entry);
 
 		//Add to new checkout list
-		dl_entry_insertEnd(new_list, curr_dl_entry);
+		dl_entry_insertEnd(list, curr_dl_entry);
 	}
+#else
+	// Approx. performance metrics:
+	//    incremental processing = 0.15 us / queue entry
+	//    overhead               = 1.65 us
+	//
+	u32                 i;
+	u32                 num_checkout;
+	dl_entry          * curr_dl_entry;
+	interrupt_state_t   curr_interrupt_state;
+
+	// If the caller is not checking out any entries or if there are no entries
+	// in the free queue, then just return
+	if ((num_tqe == 0) || (queue_free.length == 0)) { return 0; }
+
+	//
+	// NOTE:  At this point, the caller is trying to check out at least 1
+	//     entry and the free queue has at least one entry to check out.
+	//
+
+	// Stop the interrupts since we are modifying list state
+	curr_interrupt_state = wlan_mac_high_interrupt_stop();
+
+	if (num_tqe < queue_free.length) {
+		// There will be entries left in the free queue after checkout
+		//
+		// Process:
+		//   - Get the head of the free queue
+		//   - Move thru the free queue for num_tqe entries
+		//   - Update the list to add checked out entries
+		//   - Update the free queue to remove checked out entries
+		//   - Separate the lists by modifying the last entry of the
+		//     new list and the new first entry of the free queue
+		//
+		num_checkout  = num_tqe;
+		curr_dl_entry = queue_free.first;
+
+		// Move thru the list to find the last item in the checkout list
+		//     NOTE:  This iteration starts at 1 since we have already "moved"
+		//         one item to the new list.
+		for (i = 1; i < num_checkout; i++) {
+			curr_dl_entry  = dl_entry_next(curr_dl_entry);
+
+			if (curr_dl_entry == NULL) {
+				xil_printf("Free Queue list not in sync.\n");
+				return 0;
+			}
+		}
+
+		//
+		// NOTE: At this point, curr_dl_entry should be pointing to the last
+		//     entry to be checked out.  Nothing has been modified yet so it
+		//     is easy to just return zero above in case of an error.
+		//
+
+		// Update the list to add the checked out entries
+		if (list->length == 0) {
+			// Transfer portion of free queue to beginning of the list
+			list->first                  = queue_free.first;
+		} else {
+			// Transfer portion of free queue to end of the list
+			dl_entry_next(list->last)       = queue_free.first;
+			dl_entry_prev(queue_free.first) = list->last;
+		}
+
+		list->last         = curr_dl_entry;
+		list->length      += num_checkout;
+
+		// Update the free queue to remove the checked out entries
+		queue_free.first   = dl_entry_next(curr_dl_entry);
+		queue_free.length -= num_checkout;
+
+		// Modify the entries to break the two lists
+		dl_entry_next(list->last)       = NULL;
+		dl_entry_prev(queue_free.first) = NULL;
+
+	} else {
+		// The free queue will be empty after checkout
+		//
+		num_checkout = queue_free.length;
+
+		// Update the list to add the checked out entries
+		if (list->length == 0) {
+			// Transfer free queue to beginning of the list
+			list->first = queue_free.first;
+		} else {
+			// Transfer free queue to end of the list
+			dl_entry_next(list->last)       = queue_free.first;
+			dl_entry_prev(queue_free.first) = list->last;
+		}
+
+		list->last    = queue_free.last;
+		list->length += queue_free.length;
+
+		// The free queue should now be empty
+		queue_free.first  = NULL;
+		queue_free.last   = NULL;
+		queue_free.length = 0;
+	}
+
+	// Restore interrupts
+	wlan_mac_high_interrupt_restore_state(curr_interrupt_state);
+#endif
+
 	return num_checkout;
+}
+
+
+
+/**
+ * @brief Checks in multiple queue entries into the free pool
+ *
+ * The queue framework maintains a pool of free queue entries. This function will check in
+ * all queue entries from the provided list to the free pool.
+ *
+ * @param dl_list* list Pointer to doubly linked list from which queue entries will be checked in.
+ *
+ * @return Number of queue entries successfully checked in
+ */
+int queue_checkin_list(dl_list * list) {
+#if 0
+	u32       i;
+	u32       num_checkin;
+	dl_entry* curr_dl_entry;
+
+	num_checkin = list->length;
+
+	// Traverse the list and update the pointers
+	for (i = 0; i < num_checkin; i++){
+		curr_dl_entry = (list->first);
+
+		//Remove from free list
+		dl_entry_remove(list, curr_dl_entry);
+
+		//Add to new checkout list
+		dl_entry_insertEnd(&queue_free, curr_dl_entry);
+	}
+#else
+	u32                 num_checkin;
+	interrupt_state_t   curr_interrupt_state;
+
+	num_checkin          = list->length;
+	curr_interrupt_state = wlan_mac_high_interrupt_stop();
+
+	// Update the free queue
+	if (queue_free.length != 0) {
+		// Stitch lists together
+		dl_entry_prev(list->first)     = queue_free.last;
+		dl_entry_next(queue_free.last) = list->first;
+	} else {
+		// Update the first entry pointer
+		queue_free.first  = list->first;
+	}
+
+	queue_free.last    = list->last;
+	queue_free.length += num_checkin;
+
+	// Remove all elements from the list
+	list->first  = NULL;
+	list->last   = NULL;
+	list->length = 0;
+
+	wlan_mac_high_interrupt_restore_state(curr_interrupt_state);
+#endif
+
+	return num_checkin;
 }
 
 
