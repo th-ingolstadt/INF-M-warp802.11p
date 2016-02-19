6b43f63cb5faa1c96bec91f6fa64853439403f1b
welsh <welsh@dd934ca3-7c2e-414f-9084-136bfdd87501>
2016-02-19 22:47:23 +0000

    Cleaning up include from wlan_man_common.h re-factor.
    git-svn-id: http://warpproject.org/svn/WARP@5230 dd934ca3-7c2e-414f-9084-136bfdd87501

--- ReferenceDesigns/w3_802.11/c/wlan_mac_common/include/wlan_mac_misc_util.h
+++ ReferenceDesigns/w3_802.11/c/wlan_mac_common/include/wlan_mac_misc_util.h
@@ -19,76 +19,7 @@
 #ifndef WLAN_MAC_MISC_UTIL_H_
 #define WLAN_MAC_MISC_UTIL_H_
 
-
-//-----------------------------------------------
-// Common function defines
-//
-#define max(a, b)                                       (((a) > (b)) ? (a) : (b))
-#define min(a, b)                                       (((a) < (b)) ? (a) : (b))
-
-#define abs_64(a)                                       (((a) < 0) ? (-1 * a) : (a))
-
-#define sat_add16(a, b)                                 (((a) > (0xFFFF - (b))) ? 0xFFFF : ((a) + (b)))
-#define sat_add32(a, b)                                 (((a) > (0xFFFFFFFF - (b))) ? 0xFFFFFFFF : ((a) + (b)))
-
-#define sat_sub(a, b)                                   (((a) > (b)) ? ((a) - (b)) : 0)
-
-#define wlan_addr_eq(addr1, addr2)                        (memcmp((void*)(addr1), (void*)(addr2), 6)==0)
-#define wlan_addr_mcast(addr)                          ((((u8*)(addr))[0] & 1) == 1)
-
-
-//-----------------------------------------------
-// Compile-time assertion defines
-//
-#define CASSERT(predicate, file)                           _impl_CASSERT_LINE(predicate, __LINE__, file)
-
-#define _impl_PASTE(a, b)                                  a##b
-#define _impl_CASSERT_LINE(predicate, line, file) \
-    typedef char _impl_PASTE(assertion_failed_##file##_, line)[2*!!(predicate)-1];
-
-
-//-----------------------------------------------
-// PHY defines
-//
-#define WLAN_PHY_FCS_NBYTES                                4
-
-
-//-----------------------------------------------
-// Unique sequence number defines
-//
-#define UNIQUE_SEQ_INVALID	                               0xFFFFFFFFFFFFFFFF
-
-
-//-----------------------------------------------
-// Level Print function defines
-//
-#define PRINT_LEVEL                                        PL_ERROR
-
-#define PL_NONE                                            0
-#define PL_ERROR                                           1
-#define PL_WARNING                                         2
-#define PL_VERBOSE                                         3
-
-#define wlan_printf(severity, format, args...) \
-    do { \
-        if (PRINT_LEVEL >= severity){ xil_printf(format, ##args); } \
-    } while(0)
-
-
-//-----------------------------------------------
-// WLAN defines
-//
-
-//Reference: http://standards.ieee.org/develop/regauth/tut/macgrp.pdf
-#define MAC_ADDR_MSB_MASK_MCAST                            0x01
-#define MAC_ADDR_MSB_MASK_LOCAL                            0x02
-
-
-//-----------------------------------------------
-// CPU Status defines
-//
-#define CPU_STATUS_INITIALIZED                             0x00000001
-#define CPU_STATUS_EXCEPTION                               0x80000000
+#include "wlan_mac_common.h"
 
 
 //-----------------------------------------------
@@ -139,40 +70,6 @@
 #define PHY_TX_PKT_BUF_MPDU_OFFSET                        (PHY_TX_PKT_BUF_PHY_HDR_SIZE + PHY_TX_PKT_BUF_PHY_HDR_OFFSET)
 
 
-
-//-----------------------------------------------
-// Antenna mode defines
-//     NOTE:  These values are enumerated and are *not* written to PHY registers
-//
-#define RX_ANTMODE_SISO_ANTA                               0x0
-#define RX_ANTMODE_SISO_ANTB                               0x1
-#define RX_ANTMODE_SISO_ANTC                               0x2
-#define RX_ANTMODE_SISO_ANTD                               0x3
-#define RX_ANTMODE_SISO_SELDIV_2ANT                        0x4
-#define RX_ANTMODE_SISO_SELDIV_4ANT                        0x5
-
-#define TX_ANTMODE_SISO_ANTA                               0x10
-#define TX_ANTMODE_SISO_ANTB                               0x20
-#define TX_ANTMODE_SISO_ANTC                               0x30
-#define TX_ANTMODE_SISO_ANTD                               0x40
-
-#define PHY_MODE_NONHT	0x1 //11a OFDM
-#define PHY_MODE_HTMF	0x2 //11n OFDM, HT mixed format
-
-//-----------------------------------------------
-// Receive filter defines
-//     NOTE:  These filters allow the user to select the types of received packets to process
-//
-#define RX_FILTER_FCS_GOOD                                 0x1000    ///< Pass only packets with good checksum result
-#define RX_FILTER_FCS_ALL                                  0x2000    ///< Pass packets with any checksum result
-#define RX_FILTER_FCS_MASK                                 0xF000
-#define RX_FILTER_FCS_NOCHANGE                             RX_FILTER_FCS_MASK
-
-#define RX_FILTER_HDR_ADDR_MATCH_MPDU                      0x0001    ///< Pass any unicast-to-me or multicast data or management packet
-#define RX_FILTER_HDR_ALL_MPDU                             0x0002    ///< Pass any data or management packet (no address filter)
-#define RX_FILTER_HDR_ALL                                  0x0003    ///< Pass any packet (no type or address filters)
-#define RX_FILTER_HDR_MASK                                 0x0FFF
-#define RX_FILTER_HDR_NOCHANGE                             RX_FILTER_HDR_MASK
 
 
 /*********************** Global Structure Definitions ************************/
