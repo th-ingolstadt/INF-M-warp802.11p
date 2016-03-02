c2bbe048c78209c9b6c149a346700d98da3f4271
welsh <welsh@dd934ca3-7c2e-414f-9084-136bfdd87501>
2016-03-02 16:30:23 +0000

    Documented scan code.  Added ability to do passive scan with probe_tx_interval_usec = 0; Added wlan_mac_scan_resume(); Moved default values to #defines.
    git-svn-id: http://warpproject.org/svn/WARP@5244 dd934ca3-7c2e-414f-9084-136bfdd87501

--- ReferenceDesigns/w3_802.11/c/wlan_mac_high_framework/include/wlan_mac_scan.h
+++ ReferenceDesigns/w3_802.11/c/wlan_mac_high_framework/include/wlan_mac_scan.h
@@ -14,26 +14,47 @@
  *  @author Erik Welsh (welsh [at] mangocomm.com)
  */
 
+/***************************** Include Files *********************************/
 
-#ifndef WLAN_MAC_SCAN_FSM_H_
-#define WLAN_MAC_SCAN_FSM_H_
+
+/*************************** Constant Definitions ****************************/
+#ifndef WLAN_MAC_SCAN_H_
+#define WLAN_MAC_SCAN_H_
+
+
+// Scan Timing Parameters
+//     These parameters set the scan timing parameters at boot.
+//
+#define DEFAULT_SCAN_PROBE_TX_INTERVAL_USEC                20000
+#define DEFAULT_SCAN_TIME_PER_CHANNEL_USEC                 150000
+
+
+
+/*********************** Global Structure Definitions ************************/
 
 typedef struct {
-	u32 	time_per_channel_usec;
-	u32 	probe_tx_interval_usec;
-	u8* 	channel_vec;
-	u32 	channel_vec_len;
-	char*	ssid;
+    u32       time_per_channel_usec;
+    u32       probe_tx_interval_usec;
+    u8*       channel_vec;
+    u32       channel_vec_len;
+    char*     ssid;
 } scan_parameters_t;
 
-int wlan_mac_scan_init();
+
+/*************************** Function Prototypes *****************************/
+
+int  wlan_mac_scan_init();
+
 void wlan_mac_scan_set_tx_probe_request_callback(function_ptr_t callback);
+
 volatile scan_parameters_t* wlan_mac_scan_get_parameters();
+
 void wlan_mac_scan_start();
-void wlan_mac_scan_pause();
 void wlan_mac_scan_stop();
-u32 wlan_mac_scan_is_scanning();
-void wlan_mac_scan_state_transition();
+void wlan_mac_scan_pause();
+void wlan_mac_scan_resume();
+
+u32  wlan_mac_scan_is_scanning();
 
 
 #endif
