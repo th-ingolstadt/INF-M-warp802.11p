1f62991c50c06a6eb5f5f84c1ceed06a6837921a
murphpo <murphpo@dd934ca3-7c2e-414f-9084-136bfdd87501>
2017-01-13 18:39:57 +0000

    updated title block
    git-svn-id: http://warpproject.org/svn/WARP@5742 dd934ca3-7c2e-414f-9084-136bfdd87501

--- ReferenceDesigns/w3_802.11/c/wlan_mac_high_ibss/wlan_mac_ibss_uart_menu.c
+++ ReferenceDesigns/w3_802.11/c/wlan_mac_high_ibss/wlan_mac_ibss_uart_menu.c
@@ -7,10 +7,6 @@
  *          Distributed under the Mango Communications Reference Design License
  *              See LICENSE.txt included in the design archive or
  *              at http://mangocomm.com/802.11/license
- *
- *  @author Chris Hunter (chunter [at] mangocomm.com)
- *  @author Patrick Murphy (murphpo [at] mangocomm.com)
- *  @author Erik Welsh (welsh [at] mangocomm.com)
  */
 
 /***************************** Include Files *********************************/
