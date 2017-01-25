f8fbc2d965b2c0e99d73ad0ae5134e9632eed01c
murphpo <murphpo@dd934ca3-7c2e-414f-9084-136bfdd87501>
2017-01-25 21:43:13 +0000

    First attempt at moving XPAR_ defs for MAC/PHY regs
    git-svn-id: http://warpproject.org/svn/WARP@5779 dd934ca3-7c2e-414f-9084-136bfdd87501

---
+++ ReferenceDesigns/w3_802.11/c/wlan_platform_low/include/wlan_platform_low.h
@@ -0,0 +1,22 @@
+/** @file wlan_platform_low.h
+ *  @brief Platform abstraction header for CPU Low
+ *
+ *  This contains code for configuring low-level parameters in the PHY and hardware.
+ *
+ *  @copyright Copyright 2013-2017, Mango Communications. All rights reserved.
+ *          Distributed under the Mango Communications Reference Design License
+ *                See LICENSE.txt included in the design archive or
+ *                at http://mangocomm.com/802.11/license
+ *
+ *  This file is part of the Mango 802.11 Reference Design (https://mangocomm.com/802.11)
+ */
+
+
+#ifndef WLAN_PLATFORM_LOW_
+#define WLAN_PLATFORM_LOW_
+
+#include "w3_mac_phy_regs.h"
+
+
+
+#endif /* WLAN_PLATFORM_LOW_ */
