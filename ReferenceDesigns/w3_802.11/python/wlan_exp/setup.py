8e68b92507877d64651ab6bfa14000c0e715b47e
chunter <chunter@dd934ca3-7c2e-414f-9084-136bfdd87501>
2017-02-13 19:38:39 +0000

    updated python versions
    git-svn-id: http://warpproject.org/svn/WARP@5838 dd934ca3-7c2e-414f-9084-136bfdd87501

--- ReferenceDesigns/w3_802.11/python/wlan_exp/setup.py
+++ ReferenceDesigns/w3_802.11/python/wlan_exp/setup.py
@@ -13,7 +13,7 @@ def read(*paths):
 
 setup(
     name='mango-wlan-exp',
-    version='1.6.0',
+    version='1.6.1',
     description='Experiments framework for Mango 802.11 Reference Design',
     long_description=(read('README')),
     url='http://warpproject.org/w/802.11/',
