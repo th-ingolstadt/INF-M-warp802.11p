8b4d01abdcd32f5a3a601fa89e6114955ee4d074
murphpo <murphpo@dd934ca3-7c2e-414f-9084-136bfdd87501>
2017-07-11 18:59:17 +0000

    zero-padded sn string
    git-svn-id: http://warpproject.org/svn/WARP@6125 dd934ca3-7c2e-414f-9084-136bfdd87501

--- ReferenceDesigns/w3_802.11/python-dev/wlan_exp_platform_w3/__init__.py
+++ ReferenceDesigns/w3_802.11/python-dev/wlan_exp_platform_w3/__init__.py
@@ -3,7 +3,7 @@ import wlan_exp.platform as wlan_exp_platform
 w3_platform = wlan_exp_platform.WlanExpPlatform(
     name = 'WARP v3', 
     sn_regexp = '[Ww]3-a-(?P<sn>\d+)',
-    sn_str_fmt = 'W3-a-{0}'
+    sn_str_fmt = 'W3-a-{0:05}'
 )
 
 wlan_exp_platform.register_platform(w3_platform)
