04ae8e2570ba71449e59343887c9413eaade28ff
ckanibal <frustrum@pictor.uberspace.de>
2017-12-01 16:29:53 +0100

    encapsulate captured 802.11 frames in a RFtap package

--- ReferenceDesigns/w3_802.11/c-dev/wlan_mac_high_sniffer/rftap.h
+++ ReferenceDesigns/w3_802.11/c-dev/wlan_mac_high_sniffer/rftap.h
@@ -1,15 +1,39 @@
-#include <stdint.h>
+#include "xparameters.h"
+#include "stdio.h"
+#include "stdlib.h"
+#include "xtmrctr.h"
+#include "xio.h"
+#include "string.h"
+#include "xintc.h"
 
-static const uint8_t RFTAP_IP_DEFAULT[] = { 192, 168, 100, 1 };
+#include "wlan_mac_high.h"
+#include "wlan_mac_eth_util.h"
 
-struct rftap_hdr {
-    uint32_t magic;  // signature: "RFta"
-    uint32_t len32;  // length
-    uint16_t flags;  // bitfield indicating presence of fields
-    uint8_t *data;  // the rftap fields
+struct rftap_header {
+    u32 	magic;  // signature: "RFta"
+    u16 	len32;  // length
+    u16 	flags;  // bitfield indicating presence of fields
+    u32		dlt;
 } __attribute__((packed));
-typedef struct rftap_hdr rftap_hdr_t;
+typedef struct rftap_header rftap_header_t;
+ASSERT_TYPE_SIZE(rftap_header_t, 12);
 
-void rftap_frame_init(rftap_hdr_t *frame);
+struct ieee80211_radiotap_header {
+        u8        it_version;     /* set to 0 */
+        u8        it_pad;
+        u16       it_len;         /* entire length */
+        u32       it_present;     /* fields present */
+} __attribute__((__packed__));
+typedef struct ieee80211_radiotap_header radiotap_header_t;
+ASSERT_TYPE_SIZE(radiotap_header_t, 8);
+
 void rftap_init(unsigned int device_num);
-void rftap_send(void);
+
+radiotap_header_t* radiotap_frame_init(void *dst, size_t *offset);
+rftap_header_t* rftap_frame_init(void *dst, size_t *offset);
+udp_header_t* udp_frame_init(void *dst, size_t *offset);
+ipv4_header_t* ip_frame_init(void *dst, size_t *offset);
+ethernet_header_t* ethernet_frame_init(void *dst, size_t *offset);
+
+// private
+
